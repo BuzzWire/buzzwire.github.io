28f949c2d3fbcfbf7ad80affd58e46f1203603f4
BuzzWire <yordanzd@gmail.com>
2024-06-04 11:07:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 function guardarPublicacion(texto) {
   const textoPublicacion = `${texto}\n\n`;
   
-  // Crear un Blob con el contenido del texto
-  const blob = new Blob([textoPublicacion], { type: 'text/plain' });
-
-  // Crear un enlace 'a' para descargar el Blob
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = `${new Date().toISOString()}.txt`;
-  a.click();
-
   // Subir archivo de publicación al repositorio de GitHub
-  const encodedText = encodeURIComponent(textoPublicacion);
-  const apiUrl = `https://api.github.com/repos/buzzwire/buzzwire.github.io/contents/publicaciones/${new Date().toISOString()}.txt`;
+  const encodedText = btoa(textoPublicacion);
+  const apiUrl = 'https://api.github.com/repos/buzzwire/buzzwire.github.io/contents/publicaciones/' + new Date().toISOString() + '.txt';
+  
   fetch(apiUrl, {
     method: 'PUT',
     headers: {
@@ -22,7 +14,17 @@ function guardarPublicacion(texto) {
     },
     body: JSON.stringify({
       message: 'Nueva publicación',
-      content: btoa(encodedText)
+      content: encodedText
     })
   })
+  .then(response => {
+    if (response.ok) {
+      console.log('Archivo subido con éxito.');
+    } else {
+      console.error('Error al subir el archivo.');
+    }
+  })
+  .catch(error => {
+    console.error('Error al subir el archivo:', error);
+  });
 }
