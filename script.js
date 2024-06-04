de61aea2029ed899f3549a6f74d95f374e53c9f3
BuzzWire <yordanzd@gmail.com>
2024-06-04 11:04:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const email = e.target.querySelector('input[type="email"]').value;
-  const password = e.target.querySelector('input[type="password"]').value;
-  alert('Email: ' + email + '\nContraseña: ' + password);
-});
+function guardarPublicacion(texto) {
+  const textoPublicacion = `${texto}\n\n`;
+  
+  // Crear un Blob con el contenido del texto
+  const blob = new Blob([textoPublicacion], { type: 'text/plain' });
+
+  // Crear un enlace 'a' para descargar el Blob
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  a.download = `${new Date().toISOString()}.txt`;
+  a.click();
+
+  // Subir archivo de publicación al repositorio de GitHub
+  const encodedText = encodeURIComponent(textoPublicacion);
+  const apiUrl = `https://api.github.com/repos/buzzwire/buzzwire.github.io/contents/publicaciones/${new Date().toISOString()}.txt`;
+  fetch(apiUrl, {
+    method: 'PUT',
+    headers: {
+      Authorization: 'Token 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',
+      Accept: 'application/vnd.github.v3+json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      message: 'Nueva publicación',
+      content: btoa(encodedText)
+    })
+  })
+}
