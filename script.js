31e36e87920a3b9e4e8de4822d4a5f58eeca4960
BuzzWire <yordanzd@gmail.com>
2024-06-05 09:27:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
-const textFileUrl = 'https://raw.githubusercontent.com/BuzzWire/buzzwire.github.io/main/msj.txt';
+const jsonUrl = 'https://raw.githubusercontent.com/BuzzWire/buzzwire.github.io/main/msj.json';
 const variable1Input = document.getElementById('variable1');
 const variable2Input = document.getElementById('variable2');
 const saveChangesButton = document.getElementById('saveChanges');
 
-// Función para leer el archivo de texto
-async function readTextFile() {
+// Función para leer el archivo JSON
+async function readJsonFile() {
   try {
-    const response = await fetch(textFileUrl);
-    const data = await response.text();
+    const response = await fetch(jsonUrl);
+    const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Error al leer el archivo de texto:', error);
+    console.error('Error al leer el archivo JSON:', error);
   }
 }
 
-// Función para actualizar el archivo de texto
-async function updateTextFile(data) {
+// Función para actualizar el archivo JSON
+async function updateJsonFile(data) {
   try {
-    const response = await fetch(textFileUrl, {
+    const response = await fetch(jsonUrl, {
       method: 'PUT',
       headers: {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'application/json'
       },
-      body: data
+      body: JSON.stringify(data)
     });
     if (response.ok) {
-      console.log('Archivo de texto actualizado exitosamente');
+      console.log('Archivo JSON actualizado exitosamente');
     } else {
-      console.error('Error al actualizar el archivo de texto:', response.status);
+      console.error('Error al actualizar el archivo JSON:', response.status);
     }
   } catch (error) {
-    console.error('Error al actualizar el archivo de texto:', error);
+    console.error('Error al actualizar el archivo JSON:', error);
   }
 }
 
-// Cargar los valores iniciales del archivo de texto
-readTextFile().then(data => {
-  const variables = data.split('\n');
-  variable1Input.value = variables[0];
-  variable2Input.value = variables[1];
+// Cargar los valores iniciales del archivo JSON
+readJsonFile().then(data => {
+  variable1Input.value = data.variable1;
+  variable2Input.value = data.variable2;
 });
 
 // Evento para guardar los cambios
 saveChangesButton.addEventListener('click', () => {
-  const updatedData = `${variable1Input.value}\n${variable2Input.value}`;
-  updateTextFile(updatedData);
+  const updatedData = {
+    variable1: variable1Input.value,
+    variable2: variable2Input.value
+  };
+  updateJsonFile(updatedData);
 });
